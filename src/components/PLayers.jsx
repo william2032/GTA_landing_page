16dbfa4bdaa2c6b30362032afa1d106daaf18bb0
william2032 <kivindukilonzo@gmail.com>
2025-01-16 04:59:23 -0800

    added players testimonials page

--- src/components/PLayers.jsx
+++ src/components/PLayers.jsx
@@ -5,7 +5,7 @@ const PLayers = () => {
 
     <div className="mx-auto text-center max-w-screen-xl md:mx-auto lg:max-w-3xl ">
       <div className='flex flex-row justify-between'>
-        <h3 class="mb-6 text-4xl font-bold text-white">Players</h3>
+        <h3 className="mb-6 text-4xl font-bold text-white">Players</h3>
         <p className='text-blue-500 text-2xl'>1200 <br /><span className='text-white text-lg'>friends online</span></p>
       </div>
       <p
@@ -48,9 +48,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -60,9 +60,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -72,9 +72,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -84,24 +84,24 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
               <svg
-                clip-rule="evenodd"
-                fill-rule="evenodd"
+                clipRule="evenodd"
+                fillRule="evenodd"
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500"
-                stroke-linejoin="round"
-                stroke-miterlimit="2"
+                strokeLinejoin="round"
+                strokeMiterlimit="2"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44zm.678 2.033v11.904l4.707 2.505-.951-5.236 3.851-3.662-5.314-.756z"
-                  fill-rule="nonzero" />
+                  fillRule="nonzero" />
               </svg>
             </li>
           </ul>
@@ -137,9 +137,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -149,9 +149,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -161,9 +161,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -173,9 +173,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -185,9 +185,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
           </ul>
@@ -223,9 +223,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -235,9 +235,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -247,9 +247,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -259,9 +259,9 @@ const PLayers = () => {
                 fill="currentColor"
                 className="h-5 w-5 text-yellow-500">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clip-rule="evenodd" />
+                  clipRule="evenodd" />
               </svg>
             </li>
             <li>
@@ -270,12 +270,12 @@ const PLayers = () => {
                 fill="none"
                 className="h-5 w-5 text-yellow-500"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  // stroke-linecap="round"
+                  // stroke-linejoin="round"
                   d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
               </svg>
             </li>
