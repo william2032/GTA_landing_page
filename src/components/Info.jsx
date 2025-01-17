32a935bdc180c33ae3f3ef158ecd4e99d083ea34
william2032 <kivindukilonzo@gmail.com>
2025-01-17 02:58:19 -0800

    in the info component i added a video from youtube and some game information

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { gameInfos } from '../constants'
 
 const Info = () => {
   return (
-    <div id='About'>Info</div>
+    // video trailer container with  game information
+
+    <div className='container max-w-screen-xl mx-auto  info text-white font-poppins'>
+      <div className='max-w-screen-md min-h-screen'>
+        <iframe width="1200" height="500" src="https://www.youtube.com/embed/QdBZY2fkU-0"
+          title="Grand Theft Auto VI Trailer 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+
+      <div className='flex flex-row  justify-center'>
+        {gameInfos.map((gameinfo) => (
+          <div key={gameinfo.title}>
+            <h4 className='text-white text-lg '> {gameinfo.title} </h4>
+            <ul className='justify-center space-y-3 '>
+              {gameinfo.infos.map((info, index) => (
+                <li key={index}>
+                  {info.name && <span>{info.name} </span>}
+                  {info.Date && <span id='date'>{info.Date} </span>}
+                </li>
+              ))}
+            </ul>
+            <ul id='image'>
+              {gameinfo.infos.map((info, index) => (
+                <li key={index}>
+                  {info.img && <span><img src={info.img} alt="Age rating" className=' my-2 items-center  object-contain ' /></span>}
+                </li>
+              ))}
+            </ul>
+
+          </div>
+        ))}
+      </div>
+
+
+    </div>
+
   )
 }
 
